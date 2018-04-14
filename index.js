f4701360c63502142a1ba1c4372362f0d0f91509
rfpoulos <rfpoulos@outlook.com>
2018-04-14 13:06:04 -0400

    working game of life, but super slow

--- index.js
+++ index.js
@@ -42,8 +42,30 @@ let nextFrame = seed =>
             checkNeighbors(seed, indexRow, indexColumn))
     );
 
-let checkNeighbors = (seed, row, column) => 
-    "dead";
+let checkNeighbors = (seed, myRow, myColumn) => {
+    let myself = seed[myRow][myColumn];
+    let aliveCount = 0;
+    for (let currentRow = Math.max(0, myRow - 1); currentRow < Math.min(seed.length, myRow + 2); currentRow++) {
+        for (let currentColumn = Math.max(0, myColumn - 1); currentColumn < Math.min(seed[0].length, myColumn + 2); currentColumn++) {
+            if (seed[currentRow][currentColumn] === 'alive'){
+                aliveCount++;
+            };
+        };
+    };
+    if (myself === 'alive') {
+        aliveCount -= 1;
+    }
+    return checkIsAlive(myself, aliveCount);
+};
+
+let checkIsAlive = (myself, aliveCount) => {
+    if ((myself === 'alive' && aliveCount === (2 || 3)) 
+        || (myself === 'dead' && aliveCount === 3)) {
+            return 'alive';
+    } else {
+        return 'dead';
+    }
+}
 
 class Board extends React.Component {
     constructor(props) {
